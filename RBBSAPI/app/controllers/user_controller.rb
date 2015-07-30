d41d2e8aad04789cd67ce36aa4256a7c8c172bc8
calleggr <calleggr@rose-hulman.edu>
2015-07-29 23:43:28 -0500

    can't create duplicates now, read APIs for user by id, username, and email

--- RBBSAPI/app/controllers/user_controller.rb
+++ RBBSAPI/app/controllers/user_controller.rb
@@ -2,18 +2,46 @@ class UserController < ApplicationController
 	#CRU no D because deleting a user is not needed
 
 	def create
-    @user = User.new(params[:user])
-    if @user.save
-      render :json => @user.to_json
+    temp = User.where("email = ? or username = ?", params[:user][:email], params[:user][:username]).first
+    if temp
+      render :json => 'a user with that email or username already exists'.to_json
     else
-      redner :json => 'save failed'.to_json
+      @user = User.new(params[:user])
+      if @user.save
+        render :json => @user.to_json
+      else
+        redner :json => 'save failed'.to_json
+      end
     end
 	end
 
 
-	def read
-    @user = User.new
+	def find_id
+    @user = User.find(params[:id])
+    if @user
+      render :json => @user.to_json
+    else
+      render :json => "user not found".to_json
+    end
 	end
+
+  def find_email
+    @user = User.where("email = ?", params[:email]).first
+    if @user
+      render :json => @user.to_json
+    else
+      render :json => "user not found".to_json
+    end
+  end
+
+  def find_username
+    @user = User.where("username = ?", params[:username]).first
+    if @user
+      render :json => @user.to_json
+    else
+      render :json => "user not found".to_json
+    end
+  end
 
 
 	def update
--- RBBSAPI/config/routes.rb
+++ RBBSAPI/config/routes.rb
@@ -1,4 +1,7 @@
 RBBSAPI::Application.routes.draw do
   post '/user/create' => 'user#create'
-  get '/user/read:id' => 'user#read'
+  get '/user/find_id/:id' => 'user#find_id'
+  post '/user/find_email/' => 'user#find_email'
+  post '/user/find_username/' => 'user#find_username'
+  
 end
